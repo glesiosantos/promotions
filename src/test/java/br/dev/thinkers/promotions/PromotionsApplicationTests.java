373d74a1bf2d0d86288735d88f74b61b63cd3eb6
Glesio Santos <glesioss@gmail.com>
2022-02-16 15:56:08 -0300

    estrutura inicial do projeto promotions

---
+++ src/test/java/br/dev/thinkers/promotions/PromotionsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.thinkers.promotions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
